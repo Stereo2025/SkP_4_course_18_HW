ea84e9323d37dfaa736e6807dfa62dcea65b044a
Stereo2025 <teneloks@gmail.com>
2022-09-15 13:00:50 +0300

    Added readme

--- app/config.py
+++ app/config.py
@@ -3,3 +3,4 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:'  # 'sqlite:///../movies.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = True
     JSON_AS_ASCII = False
+#######################################################################################################################
